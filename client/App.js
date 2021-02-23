d7e1ed1231eae0b42053d33c4aab00aed1e333ac
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2021-02-23 12:11:39 -0300

    theme

--- client/App.js
+++ client/App.js
@@ -4,16 +4,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Provider as PaperProvider } from "react-native-paper";
+import { DefaultTheme,Provider as PaperProvider } from "react-native-paper";
 import { Provider as StoreProvider } from "react-redux";
 import store from "./Redux/Store";
 
 const Stack = createStackNavigator();
-
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#00AACF',
+    dark:"#1B263D",
+    secondary:"#E52B2B"
+  },
+};
 export default function App() {
   return (
     <StoreProvider store={store}>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen
