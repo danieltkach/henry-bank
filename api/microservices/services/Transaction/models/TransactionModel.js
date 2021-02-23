1f9588bedafe785739af59035416cee5d5ff46d0
Daniel Tkach <danieltkach@gmail.com>
2021-02-23 12:27:02 -0300

    transaction model

---
+++ api/microservices/services/Transaction/models/TransactionModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const transactionSchema = new Schema({
+		transactionType:  {
+			type: String,
+			enum : ['transfer','recharge'],
+			default: 'transfer'
+	},
+		currency: {
+			type: String,
+			required: true
+		},
+		// REVIEW *************************
+		amount: {
+			type:	String,
+			required: true
+		},
+    idSenderAccount: {
+			type: String,
+			required: true
+		},
+		idReceiverAccount: {
+			type: String,
+			required: true
+		},
+		date: {
+			type: Date,
+			default: Date.now()
+		}
+
+  });
+
+  module.exports = mongoose.model('Transaction', transactionSchema);
--- api/microservices/services/User/userController.js
+++ api/microservices/services/User/userController.js
@@ -1,4 +1,4 @@
-const User = require('./userModel')
+const User = require('./UserModel')
 
 const getUsers = (req,res) => {
     User.find()
