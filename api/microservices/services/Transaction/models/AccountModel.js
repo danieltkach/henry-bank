ff16c4160c87aa2769e82507304ecea994d813c6
Daniel Tkach <danieltkach@gmail.com>
2021-02-23 13:05:02 -0300

    account model

--- api/microservices/services/Transaction/models/AccountModel.js
+++ api/microservices/services/Transaction/models/AccountModel.js
@@ -1,4 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const transactionSchema = new Schema({
+const accountSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  CVU: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Account', accountSchema);
