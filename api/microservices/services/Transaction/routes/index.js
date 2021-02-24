0713916af01568141f6f49b3840a566e13e0e2bf
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-24 20:28:19 -0300

    index de rutas en transacciones

---
+++ api/microservices/services/Transaction/routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+// import all routers;
+
+const router = Router();
+
+//todas las rutas de transacciones
+router.use('/transaction', require('./transactionRoutes'))
+router.use('/account', require('./accountRoutes'))
+
+module.exports = router
