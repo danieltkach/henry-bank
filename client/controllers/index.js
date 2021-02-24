1ec135b222c8cd9cd47cc66771884737c02393d2
EzequielEDL <esequieledl@gmail.com>
2021-02-24 12:51:17 -0300

    first stage - fetch controllers added (user)

---
+++ client/controllers/index.js
@@ -0,0 +1,11 @@
+export const API_USER_HOST = "http://localhost:4001";
+// export const API_TRANSACTION_HOST = "http://localhost:4001";
+export const CLIENT_HOST = "http://localhost:19002";
+
+//Fetchs asociadas a User
+export const POST_REGISTER_USER = `${API_USER_HOST}/user/signup`;//#dataForm
+export const POST_LOGIN_USER = `${API_USER_HOST}/user/login`;//#dataForm
+export const PUT_USER = `${API_USER_HOST}/user/update`;//:dataId & dataForm
+export const GET_USER_BY_ID = `${API_USER_HOST}/user`;//:dataId
+export const GET_USERS = `${API_USER_HOST}/user`;
+export const GET_ADDRESS = `${API_USER_HOST}/address`;//?direccion=:queryData
