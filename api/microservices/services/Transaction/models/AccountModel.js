bd5f22985c3bfa85c3f30e3b5a8978948ba3d3c2
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-25 00:21:14 -0300

    hook para CVU de 10 digitos

--- api/microservices/services/Transaction/models/AccountModel.js
+++ api/microservices/services/Transaction/models/AccountModel.js
@@ -11,7 +11,8 @@ const accountSchema = new Schema({
     default: '' 
   },
   CVU: {
-    type: String,
+    type: Number,
+    unique: true
   },
   balance: {
     type: String,
@@ -20,5 +21,13 @@ const accountSchema = new Schema({
 });
 
 //hook
+accountSchema.pre('save', async function (next) {
+  let max = 9999999999
+  let min = 1000000000
+  let aux = Math.floor((Math.random() * (max - min + 1)) + min);
+  this.CVU = aux
+  next();
+})
+
 
 module.exports = mongoose.model('Account', accountSchema);
