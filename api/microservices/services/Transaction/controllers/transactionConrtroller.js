8312aa08f3e908bb7287662fb29021be6e94ec41
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-24 22:55:26 -0300

    cambios en crear tranfer

--- api/microservices/services/Transaction/controllers/transactionConrtroller.js
+++ api/microservices/services/Transaction/controllers/transactionConrtroller.js
@@ -6,7 +6,7 @@ const getTranfers = (req,res) =>{
     Transaction.find({idSenderAccount : req.params.id , transactionType:'transfer'} , function(err,data){
         if(err) res.status(400).send('Sin transferencias')
         let total = 0;
-        data.map(transfer =>  total += transfer.amount )
+        data.forEach(transfer =>  total += parseFloat(transfer.amount) )
         res.status(200).send('total : ' + total)
     })
 }
@@ -16,17 +16,40 @@ const getIncomes = (req,res) =>{
     Transaction.find({idReceiverAccount : req.params.id , transactionType:'transfer'} , function(err,data){
         if(err) res.status(400).send('Sin ingresos')
         let total = 0;
-        data.map(income =>  total += income.amount )
+        data.forEach(income =>  total += parseFloat(income.amount) )
         Transaction.find({idReceiverAccount: req.params.id , transactionType:'recharge'} , function(err,data){
             if(err) res.status(200).send(total)
-            data.map(income =>  total += income.amount )
+            data.forEach(income =>  total += parseFloat(income.amount) )
             res.status(200).send('total : ' + total)
         })
     })
 }
 
+const createTransfer = (req,res ) => {
+    const {currency, amount, idSenderAccount, idReceiverAccount} = req.body
+
+    const transaction = new Transaction({
+        currency,
+        amount,
+        idSenderAccount,
+        idReceiverAccount
+    })
+    transaction.save()
+        .then(trans => {
+            res.status(200).json({
+                message: 'Transfer was created',
+                transaction
+            })
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: err.message || 'Error to created transfer'
+            })
+        })
+}
 
 module.exports= {
     getTranfers,
-    getIncomes
+    getIncomes,
+    createTransfer
 }
