19a861bb36d86fed02536632189dc195945c0230
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2021-03-03 04:19:51 -0300

    trying to add variable env

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,5 +1,12 @@
-import {HOST, PORT_API_USER, PORT_API_TRANSACTION} from "@env";
-console.log(HOST)
+import { API_KEY, ANOTHER_CONFIG } from 'react-native-dotenv'
+
+// ApiClient.init(API_KEY, ANOTHER_CONFIG)
+// No estaria funcionando lo de .env :) , asi que aqui les dejo mi .env config
+// HOST=192.168.1.40
+// PORT_API_USER=4001
+// PORT_API_TRANSACTION=4002
+// PORT_API_GATEWAY=8080
+
 export const API_USER_HOST = `http://${process.env.HOST}:${process.env.PORT_API_USER}`;
 export const API_TRANSACTION_HOST = `http://${process.env.HOST}:${process.env.PORT_API_TRANSACTION}`;
 
