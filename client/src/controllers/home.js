9c91bea2a34673dc442e6f1523b03fb30e00fca2
AgustinBessone32 <bessone98@gmail.com>
2021-03-04 11:36:32 -0300

    add controller transfer income

--- client/src/controllers/home.js
+++ client/src/controllers/home.js
@@ -1,9 +1,23 @@
 import {GET_TRANSFER_SENDED, GET_TRANSFER_INCOME} from '../constants/api'
 
 export const getTransferSended = () => {
-    console.log('GET');
     return new Promise((resolve, reject) => {
       fetch(GET_TRANSFER_SENDED, {
+        method: 'GET',
+      })
+      .then(response => {
+        resolve(response.json());
+      })
+      .catch(err => {
+        console.log(err.message)
+      });
+    })
+  }
+
+
+  export const getTransferIncomes = () => {
+    return new Promise((resolve, reject) => {
+      fetch(GET_TRANSFER_INCOME, {
         method: 'GET',
       })
       .then(response => {
