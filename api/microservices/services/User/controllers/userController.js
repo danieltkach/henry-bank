7fabfdd03fe9543ffd1a41dc30cce65062cdd1ce
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2021-03-05 04:03:59 -0300

    account routesfixed

--- api/microservices/services/User/controllers/userController.js
+++ api/microservices/services/User/controllers/userController.js
@@ -5,31 +5,46 @@ const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
 const createUser = async (req, res, next) => {
-  const { name, email, lastName, codeSecurity } = req.user;
+  const { email, codeSecurity } = req.user;
   const body = { _id: req.user._id, email: req.user.email };
   const token = jwt.sign({ user: body }, 'top_secret');
   const messages = {
     message1: '',
     message2: ''
   };
+  let user;
 
-  axios.post(`http://localhost:4002/transaction/account/${req.user._id}`)
-  .then((resp) => {
-    messages.message1 = 'Registro inicial completado, cuenta asociada';
+  User.findOne({ email: email })
+  .then(userResponse => {
+    user = userResponse;
+
+    axios.post(`http://localhost:4002/transaction/account/init/${user._id}`)
+    .then((resp) => {
+      user.accounts.push(resp.data.account);
+      user.save();
+      messages.message1 = 'Registro inicial completado, cuenta asociada';
+    })
+    .catch((err) => {
+      messages.message1 = 'Error al comunicar api transaction';
+    });
+
+    nodeMailer.sendEmail({
+        name: user.name,
+        email: user.email,
+        codeSecurity })
+    .then((resp) => {
+      messages.message2 = 'Registro inicial completado';
+      res.status(200).json({ ...messages, user: user });
+    })
+    .catch((err) => {
+      messages.message2 = 'Ya existe una cuenta con este correo';
+      res.status(400).json({ ...messages })
+    });
   })
   .catch((err) => {
-    messages.message1 = 'Error al comunicar api transaction';
-  });
-
-  nodeMailer.sendEmail({ name, lastName, email, codeSecurity })
-  .then((resp) => {
-    messages.message2 = 'Registro inicial completado';
-    res.status(200).json({ ...messages, user: req.user });
+    res.status(400).json({ message: 'Usuario inexistente' });
   })
-  .catch((err) => {
-    messages.message2 = 'Ya existe una cuenta con este correo';
-    res.status(400).json({ ...messages })
-  });
+
 };
 
 const loginUser = async (req, res, next) => {
--- api/microservices/services/User/util/htmlTemplate.js
+++ api/microservices/services/User/util/htmlTemplate.js
@@ -1,5 +1,5 @@
 
-function htmlTemplate(codeSecurity) {
+function htmlTemplate(codeSecurity, name) {
   return `
   <body>
     <div style="
@@ -28,8 +28,9 @@ function htmlTemplate(codeSecurity) {
         background-color: white;
         padding: 50px;"
       >
+        <h6 style="color:#1B263D;">Hola ${name} !<h6>
         <p style="color:#1B263D;">Utiliza este codigo para activar tu cuenta de usuario en <b>Inro</b> y continuar con tu registro.</p>
-        <h1 style="color:#1B263;D">${codeSecurity}</h1>
+        <h1 style="color:#1B263;">${codeSecurity}</h1>
         <p style="color:#1B263D;">Este codigo expira en 10 minutos !.</p>
       </div>
     </div>
