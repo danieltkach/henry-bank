5b037c4668a36060e695ab7cbe0912308da3d5db
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-03-05 11:13:31 -0300

    Arreglo de cors y false en modelo de transaction

--- api/microservices/services/Transaction/index.js
+++ api/microservices/services/Transaction/index.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
-const morgan = require('morgan')
-const {dbConnection} = require("./database/config")
+const cors = require('cors');
+const morgan = require('morgan');
+const { dbConnection } = require('./database/config');
 const routes = require('./routes/index.js');
 
 let app = express();
 
 //middleware
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4002');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //routes
 app.use('/transaction', routes);
 
 //db connection
-dbConnection()
-
+dbConnection();
 
 app.listen(4002, () => {
-    console.log('Server running on 4002');
-})
+  console.log('Server running on 4002');
+});
