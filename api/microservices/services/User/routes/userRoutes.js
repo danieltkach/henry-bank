2fee90e6cf832b8e276a517955cb6bfd522e4439
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2021-03-05 11:39:24 -0300

    agrego y eliinacion de contactos

--- api/microservices/services/User/routes/userRoutes.js
+++ api/microservices/services/User/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const passport = require('passport');
-const setPassword = require('../middlewares/setPassword');
+// const setPassword = require('../middlewares/setPassword');
 
 
 router.post('/signup', passport.authenticate('signup', { session: false }), userController.createUser);
@@ -10,7 +10,7 @@ router.put('/alias/:id', userController.modifyAlias);
 router.post('/login', userController.loginUser);
 router.post('/verify', userController.verifyCodeSecurity);
 router.put('/contact/:id', userController.addContact);
-router.delete('/contact/:id', userController.deleteContact);
+router.delete('/contacts/:id', userController.deleteContact);
 router.put('/:id', userController.modifyUser);
 router.get('/:id', userController.getUser);
 router.get('/', userController.getUsers);
