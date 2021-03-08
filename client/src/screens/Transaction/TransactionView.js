1664bf48fff3fa5e29dcc67ee6cdf6cf8d99b5a0
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-03-08 14:28:16 -0300

    Inicio Transacciones

---
+++ client/src/screens/Transaction/TransactionView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Avatar } from '../../components';
+import styles from './styles';
+
+export default function TrasactionView() {
+  return <View style={{ flex: 1 }}></View>;
+}
