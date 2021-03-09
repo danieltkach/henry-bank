d621fdce03af5f4053be72db1ace06ba6e179288
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-03-09 02:48:45 -0300

    Algo se ve

--- client/src/screens/Transaction/TransactionView.js
+++ client/src/screens/Transaction/TransactionView.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Avatar } from '../../components';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { IconButton } from 'react-native-paper';
 import styles from './styles';
+import data from './data';
 
 export default function TrasactionView() {
-  return <View style={{ flex: 1 }}></View>;
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
+
+  const [transaction, setTransaction] = useState(data.transactionHistory);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 10
+      }}
+    >
+      <IconButton icon="bank-transfer" color="black" size={40} />
+      <View style={{ flex: 1, marginLeft: 10 }}>
+        <Text>{item.description}</Text>
+        <Text>{item.date}</Text>
+      </View>
+      <View
+        style={{ flexDirection: 'row', height: '100%', alignItems: 'center' }}
+      >
+        <Text
+          style={{
+            color: item.transactionType == 'recharge' ? 'green' : 'red'
+          }}
+        >
+          {item.amount} {item.currency}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={[styles.content, { flex: 1 }]}>
+      <FlatList
+        contentContainerStyle={{ marginTop: 10 }}
+        // scrollEnabled={false}
+        data={data.transactionHistory}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{ width: '100%', height: 1, backgroundColor: 'gray' }}
+            ></View>
+          );
+        }}
+      />
+    </View>
+  );
 }
