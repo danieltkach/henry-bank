0e66341a405ae981522baf8fbd51b970e46a25fe
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-03-11 16:14:18 -0300

    transactions redux
    Uh que dolor de cabeza

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,16 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import userReducer from "./userStore/userReducer";
-import accountReducer from "./accountStore/accountReducer";
+import userReducer from './userStore/userReducer';
+import accountReducer from './accountStore/accountReducer';
 
 const reducer = combineReducers({
   accountReducer,
   userReducer
 });
 
+const middleware = [thunk];
+
 const store = createStore(
   reducer,
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
